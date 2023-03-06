2e83053ceccfb5fdda844f696cb008ae10ced893
YeetingCode <120008312@sgtedu.nl>
2023-03-06 11:20:43 +0100

    6-3-2023

--- Final/Final.py
+++ Final/Final.py
@@ -7,7 +7,7 @@
 
 
 from turtle import *
-
+import math
 
 # setup variables for time, x step, gravity, initial velocity 
 
@@ -26,35 +26,46 @@ from turtle import *
 #pendown()
 #color("blue")
 
-t = float (0)
-tstep = float (0.25)    # seconds   
+
+dt = float (0.25)    # seconds   
 g = float (9.81)        # fixed accel (downwards) (m/s/s)
-delx = float (2)        # uniform motion in x axis 
+vx = float (2)        # uniform motion in x axis 
 y = float (0)             # y position 
 list_of_exits = []
 
-print("[1] eerste try")
+print("[1] maximum height with mass, speed and angle")
 while True:
     choice = input("choose one of the possibilities: ")
     if choice in ('1'):
         if choice == ('1'):
             try:
-                yvel = float(input("Enter speed (m/s): "))
+                v = float(input("Enter speed (m/s): "))
+                angl = float(input("Enter angle (degrees): "))
+                m = float(input("Enter mass of object(kg): "))
             except ValueError:
                     print("Invalid input. Please enter a number.")
                     continue
-            if yvel > 0:
+            if v > 0 and angl >= 0 and m > 0:
                 print("run the simulation for 50 seconds ")
                 # main loop -- uniform motion in x and acceleration in y axis 
-                while (t<20):
-                    t=t+tstep
-                    x=t*delx
-                    yvel = yvel - g * tstep
-                    y=y + yvel * tstep          # change in y posn = vel + time interval 
+                a = float(0)
+                vx = float(v*math.cos(math.radians(angl)))
+                vy = float(v*math.sin(math.radians(angl)))
+                Fz = float(-m*g)
+                x = float(0)
+                t = float(0)
+                while (t < 20):
+                    x += vx*dt
+                    Fresy = Fz
+                    ay = Fresy / m
+                    vy += ay*dt
+                    y += vy * dt          # change in y posn = vel + time interval 
+                    t += dt
                     
                     goto(x,y)
                     dot(2, "blue")              # draw dot at current position
                     list_of_exits.append(y)
                 print("maximum y position is",max(list_of_exits),"m")
-        
+            else:
+                print("please insert positive values")
 
